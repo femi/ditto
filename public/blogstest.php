929889ba3e1bae146f0ae5922138ae447bf49a52
dmcmillan90 <doug90@gmail.com>
2017-02-10 14:40:25 +0000

    added php files for blog so far, need fixing/tweaking

---
+++ public/blogstest.php
@@ -0,0 +1,178 @@
+<!--
+blogs
+
+.ini file should be outside www folder, not accessible to the web
+
+
+done:
+- connect to database
+- query in a function
+- retrieve user data
+- retrieve blogs
+- insert blog
+- update blog
+- delete blog
+
+functions to:
+
+- disconnect from database?
+- escape sql injection
+
+what next?
+
+- SQL injection
+- make form linked to functions
+
+
+
+-->
+
+<?php
+
+// stolen from https://www.binpress.com/tutorial/using-php-with-mysql-the-right-way/17
+function db_connect() {
+
+    // Define connection as a static variable, to avoid connecting more than once
+    static $connection;
+
+    // Try and connect to the database, if a connection has not been established yet
+    if(!isset($connection)) {
+         // Load configuration as an array. Use the actual location of your configuration file
+        $config = parse_ini_file('config.ini');
+        $connection = mysqli_connect('localhost',$config['username'],$config['password'],$config['dbname']);
+    }
+
+    // If connection was not successful, handle the error
+    if($connection === false) {
+        // Handle error - notify administrator, log to a file, show an error screen, etc.
+        return mysqli_connect_error();
+    }
+    return $connection;
+}
+
+
+function db_query($query) {
+    // Connect to the database
+    $connection = db_connect();
+
+    // Query the database
+    $result = mysqli_query($connection,$query);
+
+    return $result;
+}
+
+// escapes the user input so no SQL injection possible
+function db_quote($value) {
+    $connection = db_connect();
+    return "'" . mysqli_real_escape_string($connection,$value) . "'";
+}
+
+// An insertion query. $result will be `true` if successful
+//$result = db_query("INSERT INTO `users` (`name`,`email`) VALUES ('John Doe','john.doe@gmail.com')");
+
+// // Retrieve users data query
+// $result = db_query("SELECT * FROM users");
+// if($result === false) {
+//     // Handle failure - log the error, notify administrator, etc.
+// } else {
+//     // We successfully inserted a row into the database
+// }
+//
+// echo 'Database before insert: <br>';
+// echo '<table>';
+// while($row = $result->fetch_assoc()){
+//     echo '<tr><td> '.$row['fName'].' '.$row['lName'].' '.$row['email'] .'</td></tr>';
+// }
+// echo '</table>'
+
+
+// ---------------------------------------------------------------------------
+// FUNCTIONS
+
+function retrieve_all_blogs() {
+    // Retrieve blogs data query
+    $result = db_query("SELECT * FROM blogs");
+    if($result === false) {
+        echo mysqli_error(db_connect());
+    } else {
+        echo "<br>success<br>";
+    }
+    return $result;
+}
+
+function insert_blog($content) {
+    // Insert content into blogs table
+    // TODO check for SQL injection
+    $result = db_query("INSERT INTO blogs (content) VALUES ('" . $content . "')");
+    if($result === false) {
+        echo mysqli_error(db_connect());
+    } else {
+        echo "<br>success<br>";
+    }
+
+}
+
+function update_blog($blogId, $content) {
+    // Update a blog with new content
+    // TODO check for SQL injection
+    $result = db_query("UPDATE blogs SET content = '" . $content . "' WHERE blogId = " . $blogId);
+    if($result === false) {
+        echo mysqli_error(db_connect());
+    } else {
+        echo "<br>success<br>";
+    }
+
+}
+
+
+
+function delete_blog($blogId) {
+    // Delete a blog from the table by blogId
+    // TODO check for SQL injection
+    $result = db_query("DELETE FROM blogs WHERE blogId = " . $blogId );
+    if($result === false) {
+        echo mysqli_error(db_connect());
+    } else {
+        echo "<br>success<br>";
+    }
+}
+
+function print_blogs() {
+    echo '<br><br>';
+    echo 'Blog table: <br>';
+    echo '<table>';
+    $blogs = retrieve_all_blogs();
+    while($row = $blogs->fetch_assoc()){
+        echo '<tr><td> '.$row['blogId'].' '.$row['content'].' '.$row['createdAt'] .' '.$row['updatedAt'].'</td></tr>';
+    }
+    echo '</table>';
+    echo '<br><br>';
+}
+
+
+
+// ---------------------------------------------------------------------------
+// TESTING FUNCTIONS AREA
+
+echo "PRINTING BLOG TABLE";
+print_blogs();
+
+
+// inserting a blog
+//echo "INSERTING A NEW BLOG";
+//insert_blog("some new content");
+//print_blogs();
+
+
+// deleting a blog, this does work!
+//echo "DELETING FIRST BLOG";
+//delete_blog(1);
+//print_blogs();
+
+// update a blog
+// echo "UPDATING BLOG 3";
+// update_blog(3, "updated content");
+// print_blogs();
+
+
+?>
