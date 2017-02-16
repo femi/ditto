cfe0c800349b8eba2895c74275f071645da45d99
Femi <FemiAw@users.noreply.github.com>
2017-02-16 15:48:01 +0000

    destroy session on logout

--- public/php/home/home.php
+++ public/php/home/home.php
@@ -1,12 +1,18 @@
 <?php include ('session.php'); ?>
 <html>
   <body>
+
     Name: <?php echo $user_data["fName"] . " " . " " . $user_data["lName"] ; ?><br>
     Sex: <?php echo $user_data["sex"]; ?><br>
     DOB: <?php echo $user_data["dob"]; ?><br>
     Mobile Number: <?php echo $user_data["mobileNumber"]; ?><br>
     Email: <?php echo $user_data["email"]; ?><br>
     City: <?php echo $user_data["city"]; ?><br>
-    Description: <?php echo $user_data["description"]; ?><br>
+    Description: <?php echo $user_data["description"]; ?><br><br>
+
+    <form action="logout.php">
+      <input type="submit" value="Logout" />
+    </form>
+
   </body>
 </html>
--- public/php/home/logout.php
+++ public/php/home/logout.php
@@ -1,1 +1,9 @@
-<?php?>
+<?php
+  session_start();
+
+  // destroy the session
+  if(session_destroy()) {
+    header("Location: /hatebook/index.php"); // redirect to hompe page
+  }
+
+?>
