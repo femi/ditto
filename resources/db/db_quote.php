b073f8b3a5788e77fe1673cf7712ab181b0318f6
Geraint Ballinger <G3zz@users.noreply.github.com>
2017-02-17 17:55:58 +0000

    Completed album CRUD functions in separate encapsulated files

--- resources/db/db_quote.php
+++ resources/db/db_quote.php
@@ -1,8 +1,9 @@
 <?php
 
+require_once(realpath(dirname(__FILE__)) . "/db_connect.php");
+
 // escapes the user input so no SQL injection possible
 function db_quote($value) {
-    require_once(realpath(dirname(__FILE__)) . "../db_connect.php");
     $connection = db_connect();
     return "'" . mysqli_real_escape_string($connection,$value) . "'";
 }
