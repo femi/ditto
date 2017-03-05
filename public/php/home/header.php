9be79173498678fbeb7f8fbf3a4778c1fb1497bc
Femi <FemiAw@users.noreply.github.com>
2017-03-05 17:28:08 +0000

    delete repeated session initialisation

--- public/php/home/header.php
+++ public/php/home/header.php
@@ -1,8 +1,3 @@
-<?php
-session_start();
-$username = $_SESSION['username'];
- ?>
-
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 <link rel="stylesheet" type="text/css" href="/css/bulma.css">
 
