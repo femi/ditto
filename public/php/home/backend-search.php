dc78bd96091e9cfb2f3d736039bac2b30768f7f3
dmcmillan90 <doug90@gmail.com>
2017-03-11 15:49:25 +0000

    updated sql search so only users in a user's friend circle are visible

--- public/php/home/backend-search.php
+++ public/php/home/backend-search.php
@@ -4,9 +4,15 @@
 require (dirname(__FILE__) . '/../../../resources/db/db_connect.php');
 
 
+
 /* Attempt MySQL server connection. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
 $connection = db_connect();
+
+
+session_start();
+
+$userId = $_SESSION['userId'];
 
 // Check connection
 if($connection === false){
@@ -19,7 +25,14 @@ $query = mysqli_real_escape_string($connection, $_POST['query']);
 // search friends first name of last name like input
 if(isset($query)){
     // Attempt select query execution
-    $sql = "SELECT * FROM users WHERE fName LIKE '" . $query . "%' or lName LIKE'" . $query . "%'";
+    //$sql = "SELECT * FROM users WHERE fName LIKE '" . $query . "%' or lName LIKE'" . $query . "%'";
+    //select * from users where users.userId in (select userId from friendcircle_users where circleId = (select circleId from friendcircles where userId = 1));
+
+
+    $sql = "select * from users as u
+            inner join friendcircle_users as fcu on fcu.userId = u.userId
+            inner join friendcircles as fc on fc.circleId = fcu.circleId
+            where fc.userId = $userId and concat(fName, ' ', lName) like '%".$query."%'";
 
 
     if($result = mysqli_query($connection, $sql)){
