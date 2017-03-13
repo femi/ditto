b376df7d8940213472499ba5e9d8c3d002b18222
Femi <FemiAw@users.noreply.github.com>
2017-03-13 14:31:10 +0000

    all tagged up

--- public/php/home/home.php
+++ public/php/home/home.php
@@ -4,7 +4,7 @@
   <body>
 
     <?php include ("header.php") ?><br>
-    
+
 
     <div class="container" style="z-index: 1">
 
@@ -15,8 +15,24 @@
             <figure class="image is-250x250">
               <img class="img-rounded" src="https://s-media-cache-ak0.pinimg.com/736x/de/28/7a/de287a2e93bbe57ef5d1ec0e77c8c6a0.jpg">
             </figure>
-            <br><h3 class="title is-3"><strong><?php echo $fullname ?></strong> </h3><hr>
+            <br><h3 class="title is-3"><strong><?php echo $fullname ?></strong> </h3>
+            <div class="content">
+              <h4><?php echo $user_data['city'];?></h4>
+              <?php echo $user_data['description'];?>
+            </div><hr>
 
+            <h3 class="title is-5"><strong>Interests</strong> </h3>
+            <div id="alltags">
+              <?php
+                while ( $row = $usertags->fetch_assoc()){
+                  $name = $row['name'];
+                  echo ("<span id=\"tag_$name\" class=\"tag is-medium is-light\"><a href=\"#\">$name</a></span>");
+                }
+              ?>
+            </div>
+
+            <div class="content">
+            </div><hr>
             <h3 class="title is-5"><strong>Privacy</strong> </h3>
               <?php include ("privacy.php") ?>
             <hr>
