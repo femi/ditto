715a2e0788677b0bf4096554699111417a480641
dmcmillan90 <doug90@gmail.com>
2017-03-14 12:15:09 +0000

    search now displays message if no results found

--- public/php/friends/backend-search-user.php
+++ public/php/friends/backend-search-user.php
@@ -26,31 +26,14 @@ $query = mysqli_real_escape_string($connection, $_POST['query']);
 if(isset($query)){
 
     // get the logged in user's friends
-        $friends = db_query("SELECT * FROM users WHERE CONCAT(fName, ' ', lName) LIKE '%$query%'");
+        $users = db_query("SELECT * FROM users WHERE CONCAT(fName, ' ', lName) LIKE '%$query%'");
 
-        while($row = $friends->fetch_assoc()){
+        while($row = $users->fetch_assoc()){
 
             echo '<a href=/'.$row['username'].'>'.$row['fName'].' '.$row['lName'].'<br></a>';
 
         }
 
-    // Attempt select query execution
-
-
-    // if($result = mysqli_query($connection, $sql)){
-    //     if(mysqli_num_rows($result) > 0){
-    //         while($row = mysqli_fetch_array($result)){
-    //             // TODO: INSERT CORRECT URL FOR USER'S PROFILE
-    //             echo "<a href=".$row['username']."><p>" . $row['fName'] . " ". $row['lName'] ."</p></a>";
-    //         }
-    //         // Close result set
-    //         mysqli_free_result($result);
-    //     } else{
-    //         echo "<p>No friends found for <b>$query</b></p>";
-    //     }
-    // } else{
-    //     echo "ERROR: Could not execute $sql. " . mysqli_error($connection);
-    // }
 }
 
 // close connection
