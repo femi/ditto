f7231333c8cb4f48cea085b50265e5c12c4b4598
Geraint Ballinger <G3zz@users.noreply.github.com>
2017-03-15 14:34:52 +0000

    Improved individual photo view css

--- public/js/editCaption.js
+++ public/js/editCaption.js
@@ -1,8 +1,7 @@
 function editCaption(albumId, filename) {
-    // Load the current caption
-    var text = document.body.getElementsByClassName('caption-container')[0].getElementsByTagName('p')[0].textContent;
-    // Set the caption-container div into an input and submit button
-    var test = document.body.getElementsByClassName('caption-container')[0];
-    document.body.getElementsByClassName('caption-container')[0].innerHTML = "<input class=\"input\" type=\"text\" value=\"" + text + "\"></input>" +
-        "<a class=\"button is-info\" onclick=\"submitCaption(" + albumId + ", '" + filename + "')\">Submit</a>";
+  // Load the current caption
+  var text = document.getElementById('currentCaption').textContent;
+  // Set the caption-container div into an input and submit button
+  var test = document.body.getElementsByClassName('caption-container')[0];
+  document.body.getElementsByClassName('caption-container')[0].innerHTML = "<div class=\"level\"><div class=\"level-left\"><input class=\"input level-item\" type=\"text\" value=\"" + text + "\"></input></div><div class=\"level-right\"><a class=\"button is-primary level-item\" onclick=\"submitCaption(" + albumId + ", '" + filename + "')\">Submit</a></div></div>";
 };
