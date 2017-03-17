10fe7afd15f1bc36da1a7a125a9b9bfdf6483b44
Geraint Ballinger <G3zz@users.noreply.github.com>
2017-03-17 00:29:31 +0000

    Added album thumbnail function and example of what it can do - it probably needs to check the privacy

--- public/php/home/albums.php
+++ public/php/home/albums.php
@@ -1,3 +1,7 @@
+<?php 
+require_once("$_SERVER[DOCUMENT_ROOT]/php/albums/getAlbumThumbnails.php");
+$photos = getAlbumThumbnails($_SESSION['userId']); // an array of photo thumbnails, maximum of length 3, minimum 0
+?>
 <h5 class="title is-5">
   <span class="icon">
     <i class="fa fa-picture-o" aria-hidden="true"></i>
@@ -16,7 +20,7 @@
 </div>
 <div class="level-item">
   <figure class="image is-64x64">
-    <div class="circle button is-outlined is-info"><a href="/<?php echo $username ?>/circles">C</a></div>
+  <div class="circle button is-outlined is-info"><?php echo $photos[0] ?></div>
   </figure>
 </div>
 </div>
