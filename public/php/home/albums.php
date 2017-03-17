1df25419acd46052defc2e7d11cc48e3d2d5c672
Femi <FemiAw@users.noreply.github.com>
2017-03-17 19:23:42 +0000

    album thumbnails

--- public/php/home/albums.php
+++ public/php/home/albums.php
@@ -1,8 +1,8 @@
-<?php 
+<?php
 require_once("$_SERVER[DOCUMENT_ROOT]/php/albums/getAlbumThumbnails.php");
 $photos = getAlbumThumbnails($_SESSION['userId']); // an array of photo thumbnails, maximum of length 3, minimum 0
 ?>
-<h5 class="title is-5">
+<!-- <h5 class="title is-5">
   <span class="icon">
     <i class="fa fa-picture-o" aria-hidden="true"></i>
   </span>
@@ -10,17 +10,41 @@ $photos = getAlbumThumbnails($_SESSION['userId']); // an array of photo thumbnai
 </h5><div class="level">
   <div class="level-item">
   <figure class="image is-64x64">
-    <div class="circle button is-outlined is-info"><a href="/<?php echo $username ?>/circles">A</a></div>
+    <div class="circle button is-outlined is-info"><a href="/<?php //echo $username ?>/circles">A</a></div>
   </figure>
 </div>
 <div class="level-item">
   <figure class="image is-64x64">
-    <div class="circle button is-outlined is-info"><a href="/<?php echo $username ?>/circles">B</a></div>
+    <div class="circle button is-outlined is-info"><a href="/<?php// echo $username ?>/circles">B</a></div>
   </figure>
 </div>
 <div class="level-item">
   <figure class="image is-64x64">
-  <div class="circle button is-outlined is-info"><?php echo $photos[0] ?></div>
+  <div class="circle button is-outlined is-info"><?php //echo $photos[0] ?></div>
   </figure>
 </div>
+</div> -->
+
+
+<h5 class="title is-5">
+  <span class="icon">
+    <i class="fa fa-picture-o" aria-hidden="true"></i>
+  </span>
+  <strong>Albums</strong>
+</h5>
+<div class="level">
+
+<?php
+foreach ($photos as $photo) {
+  echo
+  "
+  <div class=\"level-item\">
+    <figure class=\"image is-64x64\">
+    <div class=\"circle button is-outlined is-info\">$photo</div>
+    </figure>
+  </div>
+  ";
+}
+?>
+
 </div>
