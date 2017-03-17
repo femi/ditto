259b65a667b9537fa1da0427a0260206abdd1900
Femi <FemiAw@users.noreply.github.com>
2017-03-17 20:27:14 +0000

    album thumbmails

--- public/php/home/albums.php
+++ public/php/home/albums.php
@@ -2,29 +2,6 @@
 require_once("$_SERVER[DOCUMENT_ROOT]/php/albums/getAlbumThumbnails.php");
 $photos = getAlbumThumbnails($_SESSION['userId']); // an array of photo thumbnails, maximum of length 3, minimum 0
 ?>
-<!-- <h5 class="title is-5">
-  <span class="icon">
-    <i class="fa fa-picture-o" aria-hidden="true"></i>
-  </span>
-  <strong>Albums</strong>
-</h5><div class="level">
-  <div class="level-item">
-  <figure class="image is-64x64">
-    <div class="circle button is-outlined is-info"><a href="/<?php //echo $username ?>/circles">A</a></div>
-  </figure>
-</div>
-<div class="level-item">
-  <figure class="image is-64x64">
-    <div class="circle button is-outlined is-info"><a href="/<?php// echo $username ?>/circles">B</a></div>
-  </figure>
-</div>
-<div class="level-item">
-  <figure class="image is-64x64">
-  <div class="circle button is-outlined is-info"><?php //echo $photos[0] ?></div>
-  </figure>
-</div>
-</div> -->
-
 
 <h5 class="title is-5">
   <span class="icon">
@@ -35,16 +12,21 @@ $photos = getAlbumThumbnails($_SESSION['userId']); // an array of photo thumbnai
 <div class="level">
 
 <?php
-foreach ($photos as $photo) {
-  echo
-  "
-  <div class=\"level-item\">
-    <figure class=\"image is-64x64\">
-    <div class=\"is-info\">$photo</div>
-    </figure>
-  </div>
-  ";
+
+if (count($photos) !== 0 ) {
+  foreach ($photos as $photo) {
+    echo
+    "
+    <div class=\"level-item\">
+      <figure class=\"image is-64x64\">
+        <div class=\"is-info\">$photo</div>
+      </figure>
+    </div>
+    ";
+  }
+} else {
+  echo "<div class=\"content\">You dont seem to have any photos at the moment, you can create some <a href=\"$username/albums\">here.</a></div>";
+
 }
 ?>
-
 </div>
--- public/php/home/circles.php
+++ public/php/home/circles.php
@@ -15,22 +15,25 @@
 
   $result = retrieve_friend_circles($userId);
 
-  if (mysqli_fetch_row($result) === 0) {
-    echo "<div class=\"content\">You dont seem to have any circles at the moment, you can create some <a href=\"#\">here.</a></div>";
+  if ($result) {
+    if (mysqli_num_rows($result) > 0 ) {
+      $counter = 0;
+      while($row = $result->fetch_assoc() and $counter < 3) {
+        $name = strtoupper(substr($row['name'], 0, 1));
+        $id = $row['circleId'];
+        echo
+        "<div class=\"level-item image is-64x64\">
+          <a class=\"circle button is-primary\" href=\"/$username/circles/$id\"><strong><h1 class=\"title is-5\">$name</h1></strong></a>
+        </div>
+        ";
+        $counter++;
+    }
+  } else {
+      echo "<div class=\"content\">You dont seem to have any circles at the moment, you can create some <a href=\"$username/circles\">here.</a></div>";
+    }
   }
 
-  $counter = 0;
-  while($row = $result->fetch_assoc() and $counter < 3) {
-    $name = strtoupper(substr($row['name'], 0, 1));
-    $id = $row['circleId'];
-    echo
 
-    "<div class=\"level-item image is-64x64\">
-      <a class=\"circle button is-primary\" href=\"/$username/circles/$id\"><strong><h1 class=\"title is-5\">$name</h1></strong></a>
-    </div>
-    ";
-    $counter++;
-  }
 
   // $counter = 0;
  ?>
