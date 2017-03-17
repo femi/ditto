6b808bb1894cbaa784743afad52526200f6778f5
Geraint Ballinger <G3zz@users.noreply.github.com>
2017-03-17 21:17:50 +0000

    Revert "Album and circle thumbnails"

--- public/php/friendCircles/friend-circles-R.php
+++ public/php/friendCircles/friend-circles-R.php
@@ -14,7 +14,7 @@ $connection = db_connect(); // the db connection
 
 // Retrieve all friend circles for a given userId
 function retrieve_friend_circles($userId) {
-
+  
     $result = db_query("SELECT * FROM friendcircles WHERE userId = $userId ORDER BY circleId DESC");
 
     if($result === false) {
