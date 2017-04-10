ac27a2b7f59f70fc2279726aa3e24905d52f952e
Femi <FemiAw@users.noreply.github.com>
2017-04-10 17:43:40 +0100

    seed on start

--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
-/usr/sbin/apache2ctl -D FOREGROUND & 
-service mysql start &
-/bin/bash
+/usr/sbin/apache2ctl -D FOREGROUND &
+service mysql start &&
+mysql < /home/ditto/seedscripts/script.sql &
+bin/bash
