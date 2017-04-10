972fd8c120a5d8e89c2e30b7d9ac96db33306f0d
Femi <FemiAw@users.noreply.github.com>
2017-04-10 20:07:44 +0100

    Update seeddb.sh

--- seedscripts/seeddb.sh
+++ seedscripts/seeddb.sh
@@ -2,3 +2,5 @@
 
 echo 'copying profile pictures to album folders..'
 cp -vr ./seedphotos/* ../resources/album_content/
+sudo chown -R $USERNAME:www-data ../resources/album_content/
+sudo chmod -R 775 ../resources/album_content/
